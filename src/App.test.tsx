76c530ecd942a42cfa63abdf9c0117c16e4f732f
yijiahe <yi.jiahe00@gmail.com>
2023-08-10 07:13:48 +0000

    Ensure augment materials are in afflicted materials

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import afflictedMaterials from './data/afflicted_materials.json'
+import weaponAugments from './data/augments.json'
 
+test('Upgrade Materials in Afflicted Materials', () => {
+    (weaponAugments as [string, string[]][][]).forEach(augment => {
+        augment.forEach(level => {
+            const materials: string[] = level[1];
+            materials.forEach(material => {
+               expect(afflictedMaterials).toContain(material); 
+            })
+        })
+    })    
+})
